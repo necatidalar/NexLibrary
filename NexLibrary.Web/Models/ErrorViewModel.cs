f83c0d224bd52a29b820ef76605de663e839a0e5
necatidalar <necatidalar5@gmail.com>
2026-05-14 00:01:50 +0300

    Gereksiz otomatik web dosyalari temizlendi

--- NexLibrary.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NexLibrary.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
